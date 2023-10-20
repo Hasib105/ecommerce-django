0c26867b04c787ca67df398b28d0286fd3a85957
hasib ahmed <hasibahmed1054@gmail.com>
2023-10-21 02:00:24 +0600

    setting urls.py

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
-
+from django.shortcuts import render , get_object_or_404
+from .models import Category, Prouct
 # Create your views here.
+
+def product_list(request, category_slug=None):
+    category = None
+    
