64a9498d1344bdfd715d1892e14952f20ffc7d85
hasib ahmed <hasibahmed1054@gmail.com>
2023-10-21 01:13:02 +0600

    Category and Product model

--- shop/models.py
+++ shop/models.py
@@ -2,4 +2,48 @@ from django.db import models
 
 # Create your models here.
 
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200,unique=True)
+    category_image = models.ImageField(upload_to='category_image', blank=True)
 
+    class Meta:
+        ordering = ['name']
+        indexes = [
+            models.Index(fields=['name']),
+        ]
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+    
+    def __str__(self):
+        return self.name
+    
+class Prouct(models.Model):
+    category = models.ForeignKey(Category,related_name='products', on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200,unique=True)
+    product_image = models.ImageField(upload_to='product_image', blank=True)
+    description = models.TextField(blank=True)
+    summary = models.CharField(max_length=300)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['name']
+        indexes = [
+            models.Index(fields=['id', 'slug']),
+            models.Index(fields=['name']),
+            models.Index(fields=['-created']), 
+        ]
+
+    def __str__(self):
+        return self.name
+
+
+
+
+
+
+        
