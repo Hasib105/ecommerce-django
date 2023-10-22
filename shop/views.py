6205d4f375fa0419287ad7922741d6d87508dade
hasib ahmed <hasibahmed1054@gmail.com>
2023-10-22 10:07:53 +0600

    create a view for show category

--- shop/views.py
+++ shop/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render , get_object_or_404
 from .models import Category, Prouct
 
 def index(request):
-    return render(request, 'shop/index.html')
+    categories = Category.objects.all()
+    return render(request, 'shop/index.html', {
+        'categories' : categories
+    })
   
-def product_list(request, category_slug=None):
-    category = None
