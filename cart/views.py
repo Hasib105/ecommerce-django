b1660202b0ed5d305e64d4890d4bcefd6cf78f63
hrmasss <hojayfarahman@gmail.com>
2023-10-28 15:07:39 +0600

    Fixed cart_add functionality

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,4 @@
+from django.http import JsonResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.decorators.http import require_POST
 from shop.models import Product
@@ -11,14 +12,14 @@ from .forms import CartAddProductForm
 def cart_add(request, product_id):
     cart = Cart(request)
     product = get_object_or_404(Product, id=product_id)
-    form = CartAddProductForm(require_POST)
+    form = CartAddProductForm(request.POST)
+
     if form.is_valid():
         cd = form.cleaned_data
-        cart.add(
-            product=product, quantity=cd["quantity"], override_quantity=cd["quantity"]
-        )
-
-    return redirect("cart:cart_details")
+        cart.add(product=product)
+        return JsonResponse({"message": "Item added to the cart"})
+    else:
+        return JsonResponse({"message": "Failed to add item to the cart"}, status=400)
 
 
 @require_POST
@@ -32,10 +33,7 @@ def cart_remove(request, product_id):
 def cart_details(request):
     cart = Cart(request)
     for item in cart:
-        item['update_quantity_for']= CartAddProductForm(initial={
-            'quantity': item['quantity'],
-            'override':True
-        })
-    return render(request, 'cart/cart_details.html', {
-        'cart': cart
-    })
+        item["update_quantity_for"] = CartAddProductForm(
+            initial={"quantity": item["quantity"], "override": True}
+        )
+    return render(request, "cart/cart_details.html", {"cart": cart})
--- static/js/script.js
+++ static/js/script.js
@@ -2,3 +2,30 @@ function toggleTheme() {
     const html = document.querySelector("html");
     html.classList.toggle("dark");
 }
+
+let forms = document.querySelectorAll(".cart-add-form");
+
+forms.forEach((form) => {
+    form.addEventListener("submit", function (event) {
+        event.preventDefault();
+
+        let formData = new FormData(this);
+
+        fetch(this.action, {
+            method: "POST",
+            body: formData,
+            headers: {
+                "X-CSRFToken": formData.get("csrfmiddlewaretoken"),
+            },
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                if (data.message) {
+                    alert(data.message);
+                }
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    });
+});
