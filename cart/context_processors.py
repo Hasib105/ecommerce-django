d771bfe4ee79ff612b1545ef9a3296e6b50e5d1c
hrmasss <hojayfarahman@gmail.com>
2023-11-01 11:40:18 +0600

    Fix cart item counter

--- cart/context_processors.py
+++ cart/context_processors.py
@@ -1,3 +1,12 @@
-from .cart import Cart
-def cart(request):
- return {'cart': Cart(request)}
+from .models import CartItem
+
+
+def cart_items_count(request):
+    session_id = request.session.session_key
+
+    cart_items = CartItem.objects.filter(session_id=session_id)
+    cart_items_count = 0
+    for item in cart_items:
+        cart_items_count += item.quantity
+
+    return {"cart_items_count": cart_items_count}
