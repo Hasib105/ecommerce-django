b417e395834720d09ab7c24a70cc840307654973
hrmasss <hojayfarahman@gmail.com>
2023-11-19 23:00:38 +0600

    Fix media | Break mobile nav

--- shop/urls.py
+++ shop/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("categories", views.categories, name="categories"),
     path("product_details/<pk>/", views.product_details, name="product_details"),
+    path("category_details/<pk>/", views.category_details, name="category_details"),
     # TODO: Delete Redundant code for testing new templates
     path("test", views.test_template),
 ]
